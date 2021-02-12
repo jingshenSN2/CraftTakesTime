ab1e652403c20e742c9650c18c608ffc22a74e59
jingshenSN2 <512320254@qq.com>
2021-02-12 13:26:00 +0800

    Update 0.3.4
    Fixed the bug 'fixed' in 0.3.3 again

--- src/main/java/sn2/timecraft/TimeCraftLoadingPlugin.java
+++ src/main/java/sn2/timecraft/TimeCraftLoadingPlugin.java
@@ -1,15 +1,10 @@
 package sn2.timecraft;
 
-import java.io.File;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.security.CodeSource;
 import java.util.Map;
 
 import org.spongepowered.asm.launch.MixinBootstrap;
 import org.spongepowered.asm.mixin.Mixins;
 
-import net.minecraftforge.fml.relauncher.CoreModManager;
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
 
 public class TimeCraftLoadingPlugin implements IFMLLoadingPlugin {
@@ -17,15 +12,6 @@ public class TimeCraftLoadingPlugin implements IFMLLoadingPlugin {
 	public TimeCraftLoadingPlugin() {
 		MixinBootstrap.init();
 		Mixins.addConfiguration("mixins.timecraft.json");
-
-		/*
-		 * CodeSource codeSource =
-		 * this.getClass().getProtectionDomain().getCodeSource(); if (codeSource !=
-		 * null) { URL location = codeSource.getLocation(); try { File file = new
-		 * File(location.toURI()); if (file.isFile()) {
-		 * CoreModManager.getReparseableCoremods().remove(file.getName()); } } catch
-		 * (URISyntaxException ignored) { } }
-		 */
 	}
 
 	@Override
