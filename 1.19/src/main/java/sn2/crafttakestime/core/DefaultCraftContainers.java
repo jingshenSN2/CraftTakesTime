f11a05831106617240d8118edd6c691e2b2f20e4
Jingsong Gao <512320254@qq.com>
2024-08-25 15:04:14 -0700

    fix(compat): add support for Curios api in inventory

--- 1.19/src/main/java/sn2/crafttakestime/core/DefaultCraftContainers.java
+++ 1.19/src/main/java/sn2/crafttakestime/core/DefaultCraftContainers.java
@@ -15,10 +15,22 @@ public class DefaultCraftContainers {
     private final List<ContainerConfig> craftContainers = new ArrayList<>();
 
     private DefaultCraftContainers() {
+        // minecraft inventory
         registerCraftContainer(ContainerConfig.builder()
                 .containerName("minecraft:inventory")
                 .ingredientSlots(SlotRange.fromString("1-4"))
                 .guiContainerClassName("net.minecraft.client.gui.screens.inventory.InventoryScreen")
+                .overlayTexture("crafttakestime:textures/gui/crafting_overlay_inventory.png")
+                .overlayX(134)
+                .overlayY(29)
+                .overlayWidth(18)
+                .overlayHeight(15)
+                .build());
+        // minecraft inventory with Curios API
+        registerCraftContainer(ContainerConfig.builder()
+                .containerName("minecraft:inventory")
+                .ingredientSlots(SlotRange.fromString("1-4"))
+                .guiContainerClassName("top.theillusivec4.curios.client.gui.CuriosScreen")
                 .overlayTexture("crafttakestime:textures/gui/crafting_overlay_inventory.png")
                 .overlayX(134)
                 .overlayY(29)
