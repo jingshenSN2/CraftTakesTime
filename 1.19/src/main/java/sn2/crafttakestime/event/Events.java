b3725573bc36e884e3ae5431f2e162cbd63d7343
Jingsong Gao <512320254@qq.com>
2024-09-04 22:30:51 -0700

    feat(command): add command to print/reload config

--- 1.19/src/main/java/sn2/crafttakestime/event/Events.java
+++ 1.19/src/main/java/sn2/crafttakestime/event/Events.java
@@ -1,7 +1,11 @@
 package sn2.crafttakestime.event;
 
+import lombok.extern.slf4j.Slf4j;
+import net.minecraft.commands.Commands;
+import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
+import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.event.server.ServerStartedEvent;
@@ -12,6 +16,7 @@ import sn2.crafttakestime.common.core.CraftManager;
 import sn2.crafttakestime.networking.PacketCraftConfig;
 import sn2.crafttakestime.networking.TimeCraftPacketHandler;
 
+@Slf4j
 @Mod.EventBusSubscriber()
 public class Events {
 
@@ -35,5 +40,33 @@ public class Events {
             return;
         }
         CraftManager.getInstance().tick();
+    }
+
+    @SubscribeEvent
+    public static void onRegisterCommand(RegisterCommandsEvent event) {
+        var dispatcher = event.getDispatcher();
+        var cmd = dispatcher.register(
+                Commands.literal("crafttakestime")
+                        .then(Commands.literal("config").requires((commandSource) -> commandSource.hasPermission(2))
+                                .then(Commands.literal("reload")
+                                        .executes((commandSource) -> {
+                                            CraftManager.getInstance().loadConfig();
+                                            PacketCraftConfig packet = new PacketCraftConfig(CraftManager.getInstance().getConfig());
+                                            TimeCraftPacketHandler.INSTANCE.send(
+                                                    PacketDistributor.ALL.noArg(),
+                                                    packet);
+                                            commandSource.getSource().sendSuccess(
+                                                    Component.literal("Config reloaded and send to all players"),
+                                                    true);
+                                            return 1;
+                                        }))
+                                .then(Commands.literal("print")
+                                        .executes((commandSource) -> {
+                                            commandSource.getSource().sendSuccess(
+                                                    Component.literal(CraftManager.getInstance().getConfig().toString()),
+                                                    true);
+                                            return 1;
+                                        }))));
+        dispatcher.register(Commands.literal("ctt").redirect(cmd));
     }
 }
