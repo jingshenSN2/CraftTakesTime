e6e56b6a89b6fee4ac14247ba9bb68777f099822
Jingsong Gao <512320254@qq.com>
2024-09-17 18:13:54 -0700

    feat(init): init commit for 1.20.1-fabric

---
+++ 1.20-fabric/src/main/java/sn2/crafttakestime/CraftTakesTime.java
@@ -0,0 +1,10 @@
+package sn2.crafttakestime;
+
+import net.fabricmc.api.ModInitializer;
+
+public class CraftTakesTime implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
